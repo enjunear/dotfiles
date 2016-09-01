89d84fe91074c53a449a5fc1803dc3ad801e3d0c
Michael Reeves <mikey.reeves@gmail.com>
2016-09-02 00:29:20 +0800

    Init commit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Deploy i3 config files and scripts to the relevant locations
+# and set the right permissions
+
+#   files           destination
+#   .Xresources     ~/
+#   .config/i3/*    ~/.config/i3/
+#   .scripts/*      /usr/share/i3blocks/
+cp .Xresources ~/
+cp .config/i3/* ~/.config/i3/
+cp .scripts/*   /usr/share/i3blocks/
+sudo chmod +x /usr/share/i3blocks/*
