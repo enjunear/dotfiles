d2d2604bca308fbbd2949ebbccbdd7e019cb9750
Michael Reeves <mikey.reeves@gmail.com>
2016-09-02 00:43:00 +0800

    housekeeping

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Deploy i3 config files and scripts to the relevant locations
 # and set the right permissions
 
@@ -6,6 +6,7 @@
 #   .Xresources     ~/
 #   .config/i3/*    ~/.config/i3/
 #   .scripts/*      /usr/share/i3blocks/
+pwd
 cp .Xresources ~/
 cp .config/i3/* ~/.config/i3/
 cp .scripts/*   /usr/share/i3blocks/
